75660781ee14f6128aac27b1d2b9b77a93d55285
ivi-dev <videviliyan@gmail.com>
2025-08-10 10:57:06 +0300

    Update run.sh

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -3,5 +3,11 @@
 CONTAINER_NAME=flags-web-fe
 
 docker rm -f $CONTAINER_NAME
-docker run -d --name $CONTAINER_NAME -p 80:80 $CONTAINER_NAME:latest
+docker run -d \
+       --name $CONTAINER_NAME \
+       -p 80:80 \
+       -p 443:444 \
+       --mount type=volume,src=flags-cert-manager-private,dst=/etc/ssl/private/flags \
+       --mount type=volume,src=flags-cert-manager-public,dst=/etc/ssl/certs/flags \
+       $CONTAINER_NAME:latest
 docker network connect flags $CONTAINER_NAME
